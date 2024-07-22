6b674da07dfc7f907e6ecfb44b000a19b7d1f301
adhii_pro <adithyen1@gmail.com>
2024-07-22 12:45:59 +0530

    bug fixing

--- gym_app/src/App.js
+++ gym_app/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css';
+import ExersiceDetail from './pages/ExersiceDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+  <Box width="400px">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exersice/:id" />
+    </Routes> 
+  </Box>  
   )
 }
 
